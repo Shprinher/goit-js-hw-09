b4f13b53b925167adf2657b3f5c32fc1da5e94a6
Shprinher <119606175+Shprinher@users.noreply.github.com>
2023-05-16 09:51:01 +0300

    make three task 09hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+import Notiflix from 'notiflix';
+const form = document.querySelector(".form");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+  let currentDelay = parseInt(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    currentDelay += parseInt(step.value);
+  }
+});
